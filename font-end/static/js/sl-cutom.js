5584df1c6ffaf008e470c35a1c61eed3c962ca16
FatinShadab <macrotech21@gmail.com>
2024-07-11 09:16:57 +0600

    Removed unnecessary files

--- font-end/static/js/sl-cutom.js
+++
@@ -1,6 +0,0 @@
-const drawer = document.querySelector('.drawer-placement-top');
-const openButton = document.querySelector('.humburger');
-const closeButton = drawer.querySelector('sl-button[variant="primary"]');
-
-openButton.addEventListener('click', () => drawer.show());
-closeButton.addEventListener('click', () => drawer.hide());
