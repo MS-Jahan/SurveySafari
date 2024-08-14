d93e70bdcf0352b8a3e9ae30ba7a99d4adaf8eef
FatinShadab <macrotech21@gmail.com>
2024-08-15 00:11:25 +0600

    configured author pages

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -5,18 +5,28 @@ import {
     setUpLogin,
     setUpSignup,
     setUpForgotPassword,
-    setUpResetPassword
+    setUpResetPassword,
+    setUpAuthorDashboard,
+    setUpAuthorSurveyArchive,
+    setUpAuthorSurvey,
+    setUpAuthorUpgradeSubscription
 } from './index.js';
 
 function initApp() {
     const page = document.body.id;
     
     switch (page) {
+        // annonimus pages
         case 'index': setUpIndex(); break;
         case 'login': setUpLogin(); break;
         case 'signup': setUpSignup(); break;
         case 'forgot_password': setUpForgotPassword(); break;
         case 'reset_password': setUpResetPassword(); break;
+        // author pages
+        case 'author_dashboard': setUpAuthorDashboard(); break;
+        case 'author_survey_archive': setUpAuthorSurveyArchive(); break;
+        case 'author_survey': setUpAuthorSurvey(); break;
+        case 'author_upgrade_subscription': setUpAuthorUpgradeSubscription(); break;
         default: console.warn('Unknown page:', page);
     }
 }
--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -3,6 +3,7 @@
 import setUpNavbar from './components/navbar.js';
 import setUpFooter from './components/footer.js';
 
+// Annonimus pages
 function setUpIndex() {
     console.log('Loading index page...');
 
@@ -50,4 +51,163 @@ function setUpResetPassword() {
     setUpFooter(0);
 }
 
-export { setUpIndex, setUpLogin, setUpSignup, setUpForgotPassword, setUpResetPassword };
+// Author pages
+function setUpAuthorDashboard() {
+    console.log('Loading author dashboard page...');
+
+    setUpNavbar({
+        homePage: '',
+        navText: 'Authors Menu 🧭',
+        navItems: [
+            {
+                link: '',
+                icon: 'bi bi-speedometer2',
+                text: 'DashBoard'
+            },
+            {
+                link: 'author_survey_archive.html',
+                icon: 'bi bi-clipboard-data',
+                text: 'Survey Archive'
+            },
+            {
+                link: '#',
+                icon: 'bi bi-chat-square-text',
+                text: 'Community Discussion'
+            },
+            {
+                link: 'author_upgrade_subscription.html',
+                icon: 'bi bi-credit-card-2-back',
+                text: 'Upgrade Subscription'
+            },
+            {
+                link: '#',
+                icon: 'bi bi-box-arrow-in-left',
+                text: 'Sign Out'
+            }
+        ]
+    }, false);
+    setUpFooter(1);
+}
+
+function setUpAuthorSurveyArchive() {
+    console.log('Loading author survey archive page...');
+
+    setUpNavbar({
+        homePage: 'author_dashboard.html',
+        navText: 'Authors Menu 🧭',
+        navItems: [
+            {
+                link: 'author_dashboard.html',
+                icon: 'bi bi-speedometer2',
+                text: 'DashBoard'
+            },
+            {
+                link: 'author_survey_archive.html',
+                icon: 'bi bi-clipboard-data',
+                text: 'Survey Archive'
+            },
+            {
+                link: '#',
+                icon: 'bi bi-chat-square-text',
+                text: 'Community Discussion'
+            },
+            {
+                link: 'author_upgrade_subscription.html',
+                icon: 'bi bi-credit-card-2-back',
+                text: 'Upgrade Subscription'
+            },
+            {
+                link: '#',
+                icon: 'bi bi-box-arrow-in-left',
+                text: 'Sign Out'
+            }
+        ]
+    }, false);
+    setUpFooter(1);
+}
+
+function setUpAuthorSurvey() {
+    console.log('Loading author survey page...');
+
+    setUpNavbar({
+        homePage: 'author_dashboard.html',
+        navText: 'Authors Menu 🧭',
+        navItems: [
+            {
+                link: 'author_dashboard.html',
+                icon: 'bi bi-speedometer2',
+                text: 'DashBoard'
+            },
+            {
+                link: 'author_survey_archive.html',
+                icon: 'bi bi-clipboard-data',
+                text: 'Survey Archive'
+            },
+            {
+                link: '#',
+                icon: 'bi bi-chat-square-text',
+                text: 'Community Discussion'
+            },
+            {
+                link: 'author_upgrade_subscription.html',
+                icon: 'bi bi-credit-card-2-back',
+                text: 'Upgrade Subscription'
+            },
+            {
+                link: '#',
+                icon: 'bi bi-box-arrow-in-left',
+                text: 'Sign Out'
+            }
+        ]
+    }, false);
+    setUpFooter(1);
+}
+
+function setUpAuthorUpgradeSubscription() {
+    console.log('Loading author upgrade subscription page...');
+
+    setUpNavbar({
+        homePage: 'author_dashboard.html',
+        navText: 'Authors Menu 🧭',
+        navItems: [
+            {
+                link: 'author_dashboard.html',
+                icon: 'bi bi-speedometer2',
+                text: 'DashBoard'
+            },
+            {
+                link: 'author_survey_archive.html',
+                icon: 'bi bi-clipboard-data',
+                text: 'Survey Archive'
+            },
+            {
+                link: '',
+                icon: 'bi bi-chat-square-text',
+                text: 'Community Discussion'
+            },
+            {
+                link: '#',
+                icon: 'bi bi-credit-card-2-back',
+                text: 'Upgrade Subscription'
+            },
+            {
+                link: '#',
+                icon: 'bi bi-box-arrow-in-left',
+                text: 'Sign Out'
+            }
+        ]
+    }, false);
+    setUpFooter(1);
+}
+
+export { 
+    setUpIndex,
+    setUpLogin,
+    setUpSignup,
+    setUpForgotPassword,
+    setUpResetPassword,
+    setUpAuthorDashboard,
+    setUpAuthorSurveyArchive,
+    setUpAuthorSurvey,
+    setUpAuthorUpgradeSubscription
+};
