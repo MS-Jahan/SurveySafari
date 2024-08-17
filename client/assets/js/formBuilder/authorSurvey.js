ba3ed7b5d9c57b4add9fed880fa2f5fb871e6903
FatinShadab <macrotech21@gmail.com>
2024-08-17 12:48:35 +0600

    Update authorSurvey.js

--- client/assets/js/formBuilder/authorSurvey.js
+++ client/assets/js/formBuilder/authorSurvey.js
@@ -1,23 +1,12 @@
+import renderForm from 'renderForm';
+
 jQuery(function ($) {
     // Initialize the formBuilder
     var formBuilder = $('#fb-editor').formBuilder();
-
-    // Function to render the form in Explorer View
-    function renderForm() {
-        var formData = formBuilder.actions.getData('json'); // Get the form data as JSON
-        if (formData && Object.keys(formData).length > 2) {
-            console.log(formData);
-            $('#rendered-form').formRender({
-                formData: formData
-            });
-        }
-        else {
-            $('#rendered-form').html('<div class="text-center my-5 py-5"><h1>No Fields Added !!!</h1></div>');
-        }
-    }
+    var formData = formBuilder.actions.getData('json'); // Get the form data as JSON
 
     // Trigger the rendering of the form when the "Explorer View" tab is clicked
     $('#explorer-view-tab').on('click', function () {
-        renderForm();
+        renderForm("rendered-form", formData);
     });
 });
