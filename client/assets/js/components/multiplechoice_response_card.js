057d1983c410356f5663c1b52d2436f54d397c37
FatinShadab <macrotech21@gmail.com>
2024-08-21 01:27:52 +0600

    Create multiplechoice_response_card.js

---
+++ client/assets/js/components/multiplechoice_response_card.js
@@ -0,0 +1,32 @@
+// assets/js/components/multiplechoice_response_card.js
+
+import { renderDonutChart } from '../chartJs/response_chart.js';
+
+function createMultipleChoiceResponseCard(responseData) {
+    const responseCard = document.createElement('div');
+    responseCard.className = 'card mb-3';
+    responseCard.style.backgroundColor = '#79AC78';
+
+    const responseCardHeader = document.createElement('div');
+    responseCardHeader.className = 'card-header';
+    responseCardHeader.innerHTML = `
+        <h4>${responseData.question}</h4>
+        <h6>Responses: #${responseData.quantity}</h6>
+    `;
+
+    const responseCardChart = document.createElement('div');
+    responseCardChart.className = 'card-body my-3 text-start';
+    responseCardChart.style.minHeight = '250px';
+    responseCardChart.innerHTML = `
+        <canvas class="mx-lg-5" id="donutChart-${responseData.id}" style="max-height: 300px;"></canvas>
+    `;
+
+    responseCard.appendChild(responseCardHeader);
+    responseCard.appendChild(responseCardChart);
+
+    renderDonutChart(`donutChart-${responseData.id}`, responseData.chartData);
+
+    return responseCard;
+}
+
+export { createMultipleChoiceResponseCard };
