2a397cceaf488f4100a555b1b0e92e9157e049c1
FatinShadab <macrotech21@gmail.com>
2024-08-20 23:23:07 +0600

    Create textfield_response_card.js

---
+++ client/assets/js/components/textfield_response_card.js
@@ -0,0 +1,27 @@
+// assets/js/components/textfield_response_card.js
+
+function createTextFieldResponseSummaryCard(textFieldResponseData) {
+    if (!textFieldResponseData) {
+        console.warn("No data provided to create text field response card.");
+        return null;
+    }
+
+    var textFieldResponseCard = document.createElement("div");
+    textFieldResponseCard.className = "card mb-3";
+    textFieldResponseCard.style.backgroundColor = "#79AC78";
+    textFieldResponseCard.innerHTML = `
+        <div class="card-header">
+            <h4>Question: ${textFieldResponseData.question}</h4>
+            <h6>Responses: #${textFieldResponseData.quantity}</h6>
+        </div>
+        <div class="card-body my-3" style="min-height: 250px;">
+            <ul class="list-group custom-scrollbar" style="overflow-y: auto; max-height: 400px;">
+                ${textFieldResponseData.responses.map(response => `<li class="list-group-item">${response}</li>`).join("")}
+            </ul>
+        </div>
+    `;
+
+    return textFieldResponseCard;
+}
+
+export { createTextFieldResponseSummaryCard };
