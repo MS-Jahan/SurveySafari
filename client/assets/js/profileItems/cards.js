f9867ff261d8486e9a05d6525f3c55a1411b6ddb
FatinShadab <macrotech21@gmail.com>
2024-08-25 00:52:34 +0600

    Update cards.js

--- client/assets/js/profileItems/cards.js
+++ client/assets/js/profileItems/cards.js
@@ -1,9 +1,25 @@
-import createDlineCard from "./dline_card.js"
+// assets/js/profileItems/cards.js
+
 import createDefaultCard from "./default_card.js"
+import createDlineCard from "./dline_card.js"
+import createDarkFallCard from "./dark_fall_card.js"
+import createGradientCard from "./gradient_card.js"
+import createHlineCard from "./hline_card.js"
+import createCloudCard from "./cloud_card.js"
+import createSnowCard from "./snow_card.js"
+import createSqureCard from "./squre_card.js"
+import createCheckCard from "./check_card.js"
 
 const profileCards = {
     "DefaultCard": createDefaultCard,
-    "DLineCard" : createDlineCard,
+    "DLineCard": createDlineCard,
+    "DarkFallCard": createDarkFallCard,
+    "GradientCard": createGradientCard,
+    "HlineCard": createHlineCard,
+    "CloudCard": createCloudCard,
+    "SnowCard": createSnowCard, 
+    "SqureCard": createSqureCard,
+    "CheckCard": createCheckCard,
 }
 
 export default profileCards;
