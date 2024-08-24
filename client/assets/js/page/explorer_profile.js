20ce9a92398e4f83c7857781693252e7b13954f1
FatinShadab <macrotech21@gmail.com>
2024-08-25 01:59:02 +0600

    Update explorer_profile.js
    made the item collection modal dynamic and implemented button logic

--- client/assets/js/page/explorer_profile.js
+++ client/assets/js/page/explorer_profile.js
@@ -1,5 +1,6 @@
 // assets/js/page/explorer_profile.js
 import profileCards from "../profileItems/cards.js"
+import createExplorerItemCard from "../components/explorer_item_card.js"
 
 function __attachProfileCard(profileData, itemKey) {
     const cardContainer = document.getElementById('profile_card_container');
@@ -13,7 +14,77 @@ function __attachProfileCard(profileData, itemKey) {
     cardContainer.appendChild(profileCards[itemKey](profileData));
 }
 
-function __loadExplorerItems() {
+function __loadExplorerItems(profileData, attachedProfileCard) {
+    const itemContainerModal = document.getElementById("itemsCollectionModal");
+    const itemContainer = document.getElementById("items_container");
+
+    if (!itemContainer || !itemContainerModal) {
+        console.log("Item Container or Modal not found!");
+        return null;
+    }
+
+    var textExplorerItems = [
+        {
+            imgUrl: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            itemName: "DLineCard"
+        },
+        {
+            imgUrl: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            itemName: "DarkFallCard"
+        },
+        {
+            imgUrl: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            itemName: "GradientCard"
+        },
+        {
+            imgUrl: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            itemName: "HlineCard"
+        },
+        {
+            imgUrl: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            itemName: "CloudCard"
+        },
+        {
+            imgUrl: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            itemName: "SnowCard"
+        },
+        {
+            imgUrl: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            itemName: "SqureCard"
+        },
+        {
+            imgUrl: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            itemName: "CheckCard"
+        }
+    ];
+
+    textExplorerItems.forEach(element => {
+        var item = createExplorerItemCard(element);
+        console.log(item);
+        var attachBtn = item.querySelector("#attach_btn");
+        var detachBtn = item.querySelector("#detach_btn");
+
+        attachBtn.disabled = false || (attachedProfileCard == attachBtn.value);
+        detachBtn.disabled = !attachBtn.disabled;
+
+        attachBtn.addEventListener("click", function() {
+            __attachProfileCard(profileData, attachBtn.value);
+
+            attachBtn.disabled = !attachBtn.disabled;
+            detachBtn.disabled = !attachBtn.disabled;
+            itemContainerModal.querySelector('.btn-close').click();
+        });
+
+        detachBtn.addEventListener("click", function() {
+            __attachProfileCard(profileData, "DefaultCard");
+
+            detachBtn.disabled = !detachBtn.disabled;
+            attachBtn.disabled = !detachBtn.disabled;
+            itemContainerModal.querySelector('.btn-close').click();
+        });
+
+        itemContainer.appendChild(item);
+    });
     
 }
 
@@ -27,7 +98,7 @@ function loadExplorerProfile() {
     }
 
     __attachProfileCard(testProfileData, attachedProfileCard);
-    __loadExplorerItems();
+    __loadExplorerItems(testProfileData, attachedProfileCard);
 }
 
 export { loadExplorerProfile };
